9aa8d1d1f51bde837a2d7f691a70fab3eb0638d8
DianaWMD <diana.ied@itas.ca>
2024-04-08 09:06:24 -0700

    eval issue solved

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -29,9 +29,16 @@ function ContributionsHandler(db) {
 
         /*jslint evil: true */
         // Insecure use of eval() to parse inputs
-        const preTax = eval(req.body.preTax);
-        const afterTax = eval(req.body.afterTax);
-        const roth = eval(req.body.roth);
+        // const preTax = eval(req.body.preTax);
+        const preTax = parseInt(req.body.preTax, 10);
+
+        // const afterTax = eval(req.body.afterTax);
+        // const roth = eval(req.body.roth);
+        let afterTax = parseInt(req.body.afterTax, 10);
+
+
+        let roth = parseInt(req.body.roth, 10);
+
 
         /*
         //Fix for A1 -1 SSJS Injection attacks - uses alternate method to eval
